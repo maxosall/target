0e8ce41a3002bd8a175d02fde260253e13b10e29
sallmaxo <sallmaxo.pro.12@gmail.com>
2023-10-13 18:39:07 +0200

    create SearchBox Component

--- target.client/app/job-opennings/page.tsx
+++ target.client/app/job-opennings/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import { SearchBox } from '../components/SearchBox'
 const JobOpennings = () => {
   return (
     <div className='grid grid-cols-12 space-y-5 md:space-y-0 md:space-x-5 '>
       <div className="col-span-12 md:col-span-3">
-        sidebar menu
+        side bar
       </div>
       <div className='col-span-12 md:col-span-9'>
-        main
+        <div className='p-5 bg-white'>
+          <SearchBox />
+        </div>
       </div>
     </div>
   )
