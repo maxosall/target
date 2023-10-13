cbc5a7e164651b20ea51679362c20a8f17c44c92
sallmaxo <sallmaxo.pro.12@gmail.com>
2023-10-13 19:28:52 +0200

    enhencement

--- target.client/app/components/Navbar.tsx
+++ target.client/app/components/Navbar.tsx
@@ -14,14 +14,13 @@ export const Navbar = () => {
 <nav className="bg-white border-gray-200 dark:bg-gray-900 border-b mb-5">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="https://flowbite.com/" className="flex items-center space-x-4">
-      {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" /> */}
-      <BiTargetLock className="h-12 w-12 text-red-500 hover:text-red-700 "></BiTargetLock>
-      <span className="self-center text-2xl text-red-500 hover:text-red-700 font-semibold whitespace-nowrap dark:text-white">target</span>
+      <BiTargetLock className="h-9 w-9 text-red-500 hover:text-red-700 "></BiTargetLock>
+      <span className="self-center text-lg text-red-500 hover:text-red-700 font-black whitespace-nowrap dark:text-white">target</span>
   </a>
   <div className="flex md:order-2 space-x-2">
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-sm rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Sign up</button>
-        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-sm rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Login</button>
       <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
@@ -31,7 +30,7 @@ export const Navbar = () => {
     </button>
   </div>
   <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+    <ul className="flex flex-col font-sm p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
      
      {links.map(link => <Link key={link.href} href={link.href} > { link.label }</Link>)}
      
--- target.client/app/job-opennings/page.tsx
+++ target.client/app/job-opennings/page.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { SearchBox } from '../components/SearchBox'
 const JobOpennings = () => {
+  const filter_list = [
+    {'id':1, 'contries': ['senegal', 'mali', 'france', 'usa', 'uk', 'egypt']},
+    {'id':2,'education level': ['all', 'none', 'high education']},
+    {'id':3, 'industry': ['software development', 'office', 'music', 'finance']},
+    {'id':4,'gender': ['male', 'female']}
+  ]  
   return (
     <div className='grid grid-cols-12 space-y-5 md:space-y-0 md:space-x-5 '>
       <div className="col-span-12 md:col-span-3">
-        side bar
+        <div className='bg-white'>
+          <h3 className='font-semibold p-5 text-gray-700'>Enhence your search</h3>
+          <aside className='px-5'>
+            <ul>
+              {/* {filter_list.map(f_list => <li key={f_list.id}>
+                
+              </li>)} */}
+            </ul>
+          </aside>
+
+        </div>
       </div>
       <div className='col-span-12 md:col-span-9'>
         <div className='p-5 bg-white'>
