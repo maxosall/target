fb1deb980707e49cc184a5b5d5e432156044142b
maxosall <sallmaxo.pro.12@gmail.com>
2023-10-23 23:40:50 +0300

    create setup target.vue with nuxt and tailwindcss

---
+++ target.vue/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <Navbar/>
+  <main>
+    <!-- <NuxtWelcome /> -->
+  </main>
+  <!-- <script src="../path/to/flowbite/dist/flowbite.min.js"></script> -->
+</template>
+
+
+
+<style>
+@import url("~/assets/css/style.css");
+</style>
