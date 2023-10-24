6e8a868744cb18c796fe0527e6942a5b61bdb52b
maxosall <sallmaxo.pro.12@gmail.com>
2023-10-24 16:48:39 +0300

    add icons

--- target.vue/components/Navbar.vue
+++ target.vue/components/Navbar.vue
@@ -1,5 +1,6 @@
 
 <script setup>
+import {Icon } from '@iconify/vue'
 const links = [
   { label: "Dashboard", href: "/dashboard" },
   { label: "Job Opennings", href: "/job-opennings" },
@@ -10,22 +11,24 @@ const links = [
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900 border-b mb-5">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="https://flowbite.com/" class="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-      </a>
+      <NuxtLink to="https://flowbite.com/" 
+        class="flex font-extrabold text-red-500 hover:text-red-700 justify-center space-x-4 items-center">
+        <Icon icon="mdi:target" class="h-9 w-9 self-center "></Icon>
+        
+        <span class="self-center text-2xl font-extrabold whitespace-nowrap dark:text-white">Flowbite</span>
+      </NuxtLink>
       <div class="flex items-center space-x-2 md:order-2">
-        <a
-            href="/account/signup"
+        <NuxtLink
+            to="/account/signup"
              class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-semibold rounded-md text-sm px-3.5 py-2 mr-3 md:mr-0  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
             Sign up
-            </a>
-          <a
-            href="/account/login"
+            </NuxtLink>
+          <NuxtLink
+            to="/account/login"
             class="text-white font-semibold bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-sm rounded-md text-sm px-3.5 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Login
-          </a>
+          </NuxtLink>
         <button data-collapse-toggle="navbar-sticky" type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-sticky" aria-expanded="false">
@@ -34,6 +37,8 @@ const links = [
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M1 1h15M1 7h15M1 13h15" />
           </svg>
+
+          
         </button>
       </div>
       <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
@@ -41,7 +46,7 @@ const links = [
           class="w-full flex flex-col font-normal text-sm p-4 md:p-0 mt-4 rounded-md bg-white md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li v-for="link in links"
             class="text-gray-900 block pl-3 pr-4 py-2 rounded-md hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-            <a href="{{ link.href }}">{{ link.label }}</a>
+            <NuxtLink :to="link.href" >{{ link.label }}</NuxtLink>
           </li>
         </ul>
       </div>
