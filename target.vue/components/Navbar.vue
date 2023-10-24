a2f9f8de14a35949bd0d35a3009c834d10068a9b
maxosall <sallmaxo.pro.12@gmail.com>
2023-10-24 20:24:14 +0300

    toggle Navbar vue

--- target.vue/components/Navbar.vue
+++ target.vue/components/Navbar.vue
@@ -5,8 +5,11 @@ const links = [
   { label: "Dashboard", href: "/dashboard" },
   { label: "Job Opennings", href: "/job-opennings" },
   { label: "Browse Jobs", href: "/browse-job" },
-  { label: "Profile", href: "/account/profile" },
+  { label: "Profile", href: "/account/profile" },  
 ];
+
+let isCollapsed = ref(false)
+
 </script>
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900 border-b mb-5">
@@ -29,19 +32,17 @@ const links = [
           >
             Login
           </NuxtLink>
-        <button data-collapse-toggle="navbar-sticky" type="button"
+        <button @click="isCollapsed = !isCollapsed" data-collapse-toggle="navbar-sticky" type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-sticky" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
-          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15" />
-          </svg>
+          
+          <Icon  icon="material-symbols:menu" class="h-5 w-5"/>
 
           
         </button>
       </div>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+      <div :class="[isCollapsed ? 'flex' : 'hidden' ,'items-center justify-between w-full md:flex md:w-auto md:order-1']" id="navbar-sticky">
         <ul
           class="w-full flex flex-col font-normal text-sm p-4 md:p-0 mt-4 rounded-md bg-white md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li v-for="link in links"
