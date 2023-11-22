ec61f0a27e7617b4a66c8a703a754e26b10f9e84
maxosall <sallmaxo.pro.12@gmail.com>
2023-11-22 19:00:08 +0200

    update_job endpoint added

--- target.fastAPI/sql_app/routers/job.py
+++ target.fastAPI/sql_app/routers/job.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, status, HTTPException, APIRouter
+from fastapi import Depends, Response, status, HTTPException, APIRouter
 from http.client import HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -18,3 +18,47 @@ def create_job(job: schema.JobCreate, db: Session=Depends(get_db),
   db.commit()
   db.refresh(new_job)
   return new_job
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def deflete_job(id:int, db: Session= Depends(get_db), 
+                current_user: int = Depends(oauth2.get_current_user)):
+  job = db.query(models.Job).filter(models.Job.Id == id).first()
+
+  if job == None:
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                        detail=f"Job with id: ({id}) does not exist")
+  
+  if job.user_id == current_user.id:
+    raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, 
+                        detail=f"Not authorized to perform the requested action")
+
+  job.delete(Synchronize_seasion = False)
+  db.commit()
+  return Response(status_code = status.HTTP_202_ACCEPTED)
+
+@router.put("/", status_code=status.HTTP_201_CREATED, response_model=schema.JobOutput)
+def update_job(updated_job: schema.JobCreate, db: Session=Depends(get_db),
+          current_user:int= Depends(oauth2.get_current_user)):
+  job = db.query(models.Job).filter(models.Job.Id == id).first()
+
+  if job == None:
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                        detail=f"Job with id: ({id}) does not exist")
+  
+  if job.user_id == current_user.id:
+    raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, 
+                        detail=f"Not authorized to perform the requested action")
+
+  job.update(updated_job.dict(), Synchronize_seasion= False)
+  db.commit()
+  return job
+
+
+  
+
+
+
+
+
+
+
